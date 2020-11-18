87a1c048fe229cf6c87545383f5fb389399e2afe
mahdi hajian <mahdi.haj.1375@gmail.com>
2020-11-18 22:58:22 +0330

    add mah app

---
+++ notePade/Window2.xaml.cs
@@ -0,0 +1,34 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace notePade
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class Window2 : MetroWindow
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+        private void LaunchGitHubSite(object sender, RoutedEventArgs e)
+        {
+            // Launch the GitHub site...
+        }
+        private void DeployCupCakes(object sender, RoutedEventArgs e)
+        {
+            // deploy some CupCakes...
+        }
+    }
+}
